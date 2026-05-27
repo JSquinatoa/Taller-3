35b695b4b1f38206cf018a8f43ebb87b0ed7ccea
Anderson <andersonjoelchancusi@gmail.com>
2026-05-27 10:56:00 -0500

    Clase 27/05/2026 Funcional

---
+++ 03.spring-ai/src/main/java/com/programacion/taller3/SpringAiMain.java
@@ -0,0 +1,14 @@
+package com.programacion.taller3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMain.class, args);
+    }
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "Taller"
 include("01.embeddings")
 include("02.langchain4j")
 include("02.langchain4j")
+include("03.spring-ai")
